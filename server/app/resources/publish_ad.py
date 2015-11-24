cb3875ad16b95315c8314bf698deb495021189a0
qitianchan <qitianchan@sina.com>
2015-11-24 20:33:43 +0800

    进度条，后台完成，前台为完成

--- server/app/resources/publish_ad.py
+++ server/app/resources/publish_ad.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
+from __future__ import division
 from server.app.extensions import auth, bcrypt
 from flask_restful import Resource
 from flask import request, jsonify, g
 from flask_restful import fields
 import os
 import websocket
-
-import thread
+from uuid import uuid4
 import time
 import json
-import redis
 import threading
 from server.app.utils import strict_redis
 from server.app.config import LORIOT_WEBSOCKET_URL as ws_url
+from redis import StrictRedis
+from server.app.config import REDIS_DB, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+from datetime import datetime
+
 
 PACKET_SIZE = 15
 
-r = strict_redis
+# r = strict_redis
+
 
 class Publish(Resource):
 
@@ -31,13 +35,12 @@ class Publish(Resource):
             # if ws == None:
             #     ws = _connet_socket(ws_url)
             chunks = slipe_file(f, PACKET_SIZE)
+            process_code = uuid4().hex
 
-            new_thread = threading.Thread(target=send_file, args=(ws, r, chunks, euis))
+            new_thread = threading.Thread(target=send_file, args=(ws, chunks, euis, process_code))
             print '开始新的线程...'
             new_thread.start()
-
-
-            return '上传成功', 201
+            return jsonify({'process_code': process_code})
         else:
             return '', 303
 
@@ -88,15 +91,19 @@ def slipe_file(file, step):
     return chunks
 
 
-def send_file(ws, redis_conn, chunks, euis):
+def send_file(ws, chunks, euis, progress_code):
     """
     发送文件
     :param ws: websocket
     :param redis_conn: redis连接
     :param file: 待发送的文件
     :param euis: 发送的eui列表
+    :param progress_code: 进度信息标志码
     :return:
     """
+    start = datetime.now()
+
+    redis_conn = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password= REDIS_PASSWORD)
 
     if not ws:
         ws = _connet_socket(ws_url)
@@ -104,11 +111,11 @@ def send_file(ws, redis_conn, chunks, euis):
     packet_indexs = dict()
 
     if euis:
-        # euis = euis.split(',')
-        # TODO
+        all_packet_be_send = len(chunks) * len(euis)            # 进度衡量量，总需要的进度
         # 初始化index
         for x in xrange(len(euis)):
             packet_indexs[euis[x]] = -1
+
 
         print '正在接收消息。。。'
         pubsub = redis_conn.pubsub()
@@ -122,6 +129,7 @@ def send_file(ws, redis_conn, chunks, euis):
 
             if done_count >= len(euis):         # 如果全部已完成，停止
                 print '发送完成'
+                pubsub.close()
                 break
 
             if recv_data.get('h'):
@@ -142,59 +150,31 @@ def send_file(ws, redis_conn, chunks, euis):
 
                     # 若已经复位，确认index已归零，如若未归零，继续等待
                     if packet_indexs[eui] == 0 and index != 0:
-                            continue
+                        reset_data = wrap_data('00', eui, index=0, end=True)
+                        ws.send(reset_data)
+                        continue
 
                     # 判断是否已经发送完最后一个包,是的话，不做处理
                     # index = packet_indexs[eui]
                     if index >= len(chunks):
                         done_count += 1
                         continue
-                #
-                #     # 包装好要发送的数据格式
-                #     send_data = wrap_data(chunks[index], eui, index, end=(index == (len(chunks) - 1)))
-                #     print u'发送数据：' , send_data
-                #     # send_data = '{"data": "04c43584247313656312e3000000000000", "cmd": "tx", "EUI": "BE7A000000000301", "port": "1"}'
-                #     print 'index:', index
-                #     ws.send(send_data)
-                #     packet_indexs[eui] += 1
-                #
-                # elif data[:2] == 'a2' or data[:2] == 'A2':
-                    # 重发数据，index为数据指定的index, 并重置各个eui的 packet index
+                    # 发送数据，index为数据指定的index, 并重置各个eui的 packet index
                     send_data = wrap_data(chunks[index], eui, index, end=(index == (len(chunks))))
 
                     print u'发送数据：' , send_data
                     print 'index:', index + 1
                     ws.send(send_data)
                     packet_indexs[eui] = index + 1
+                    # 记录进度
+                    current_progress = progress(packet_indexs, all_packet_be_send)
+                    print '=' * 60
+                    print '当前进度：', current_progress
+                    print 'progress_code', progress_code
+                    print '=' * 60
+                    publish_progress(redis_conn, progress_code, current_progress)
 
             # TODO： 记录完成状态
-
-
-def listen_redis(ws, connect_redis, euis):
-    """
-    :param ws:
-    :param connect_redis:
-    :param euis:
-    :return:
-    """
-    pubsub = r.pubsub()
-    pubsub.subscribe(euis)
-    print 'Listing...'
-
-    while True:
-        if ws == None:
-            ws = _connet_socket(ws_url)
-        print '开始接收消息'
-        recv_data = json.dumps(ws.recv())
-        print recv_data
-
-    count = 0
-    for item in pubsub.listen():
-        print item['data']
-        count += 1
-        if count > 5:
-            print '发送结束'
-            break
 
 
 def recv_redis_message(redis_conn, euis):
@@ -205,15 +185,47 @@ def recv_redis_message(redis_conn, euis):
             print item['data']
             # todo: 处理接收到的信息
 
+
 def get_index(data):
+    """
+    :param data:收到的信息
+    :return: 信息中的序号
+    """
     return int(data[2:4], 16)
+
+
+def publish_progress(redis_conn, progress_code, progress, ex_time=3600):
+    """
+    :param redis_conn: redis connection
+    :param progress_code: 进度标识码
+    :param progress: 进度（0-100）， 100 表示完成
+    :param ex_time: 存在时间，默认一个小时
+    :return:
+    """
+    res = redis_conn.get(progress_code)
+    redis_conn.set(progress_code, progress)
+    if not res:
+        redis_conn.expire(progress_code, ex_time)
+
+
+def progress(packet_indexs, all):
+    """
+    :param packet_indexs: 现在已发送的各个eui对应的文件序号
+    :param all: 所以的序号
+    :return: 进度（0 - 100）
+    """
+    values = packet_indexs.values()
+    sum = 0
+    for v in values:
+        sum += v
+    return int((sum / all) * 100)
+
 
 if __name__ == '__main__':
     ws = websocket.WebSocket()
     ws_url = 'wss://ap1.loriot.io/app?id=be7a009f&token=Rd6c66b0j2xi98cG6DW0Kg'
     ws.connect(ws_url)
     send_data = {'cmd': 'tx', 'EUI': 'BE7A000000000301', 'port': '1', 'data': '043412'}
-    send_data = {"data": "8600000000000000000000000000000060000000000000000000007a00010302", "cmd": "tx", "EUI": "BE7A000000000301", "port": LORIOT_PORT}
     while True:
         ws.send(json.dumps(send_data))
         print '发送完成', json.dumps(send_data)
--- server/test/restful_test.py
+++ server/test/restful_test.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
-import requests
-import json
+from threading import Thread
+from time import sleep
 
-with open('1HelloWorld.TXT', 'rb') as f:
-    while True:
-        chunk = f.read(15)
-        chunk = chunk.encode('hex')
-        if not chunk:
-            break
+class Timeout(Thread):
+    def __init__(self):
+        super(Timeout, self).__init__()
+        self.exitcode = 0
+        self.exception = None
+        self.ext_traceback = ''
+
+    def run(self):
+        try:
+            sleep(2)
+            print 'sleeppppy'
+            raise Exception
+        except Exception, e:
+            self.exitcode = -1
+
+
+if __name__ == '__main__':
+    t1 = Timeout()
+    t1.start()
+    t1.join()
+    if t1.exitcode != 0:
+        raise Exception
+    sleep(100)
+    print '主线程'
