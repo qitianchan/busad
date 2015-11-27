dcf5a0f426c68f17b72d7f46ea0c261da717a9c8
qitianchan <qitianchan@sina.com>
2015-11-27 16:26:36 +0800

    编辑公交

--- client/js/controllers/RouteCtrl.js
+++ client/js/controllers/RouteCtrl.js
@@ -51,21 +51,66 @@
           }
         };
 
-        $scope.saveBus = function(data, bus){
+        $scope.saveBus = function(data, bus, index){
             $scope.waiting = true;
+            var originData = {};
+            originData.plate_number = bus.plate_number;
+            originData.light_number = bus.light_number;
+            originData.route_id = bus.route_id;
+            originData.eui = bus.eui;
 
-            Bus.one(bus.id).customPUT(data).then(function(ret){
+            if(bus.id){
+                Bus.one(bus.id).customPUT(data).then(function(ret){
                 $scope.waiting = false;
                 toaster.pop('success', '保存成功', '');
-            },function(ret){
-                $scope.buses = $scope.original_buses;
-                toaster.pop('error', '保存失败', '数据冲突');
-                return ''
-            });
+                },function(ret){
+                    //$scope.buses = $scope.original_buses;
+                    bus.plate_number = originData.plate_number;
+                    bus.light_number = originData.light_number;
+                    bus.route_id = originData.route_id;
+                    bus.eui = originData.eui;
+
+                    toaster.pop('error', '保存失败', '数据冲突');
+                    return ''
+                });
+            }
+            else {
+
+
+                Bus.post(data).then(function(ret){
+                    //$scope.buses.splice(index, 1);
+                    bus.id = ret.id;
+                toaster.pop('success', '','新建成功');
+                }, function(ret){
+                    $scope.buses.splice(index, 1);
+                    toaster.pop('error', '', '新建失败')
+                });
+            }
+
             var hel = 'haha';
         };
 
         /* TODO：save, edit del add */
+        $scope.addBus = function(){
+             $scope.inserted = {
+              plate_number: null,
+              light_number: null,
+              route_id: null,
+              eui: null
+            };
+            $scope.buses.push($scope.inserted);
+        };
 
+        $scope.removeBus = function(index, bus){
+            if(!bus.id){
+                $scope.buses.splice(index, 1)
+            }
+            Bus.one(bus.id).customDELETE().then(function(ret){
+                $scope.buses.splice(index, 1);
+                toaster.pop('success', '', '删除成功');
+            },function(ret){
+                toaster.pop('error', '', '删除失败')
+            })
+        }
 }]);
 }();
