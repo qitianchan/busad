6f5b535db77e46a6416087d28029180a343b6353
qitian <qitianchan@sina.com>
2015-12-29 14:42:14 +0800

    gevent WSGIServer

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+# from gevent.monkey import patch_all; patch_all()
 from server.app.main import create_app
+from gevent.pywsgi import WSGIServer
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    WSGIServer(('', 5000), app).serve_forever()
+    # app.run(debug=True)
